0e3e4288672203314083178e303c6a08b9124896
Mayhan_GT <topchanyuk29@gmail.com>
2025-08-04 22:51:37 +0300

    Reinit with new gitignore

--- obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MoreAndQuickLoadouts
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MoreAndQuickLoadouts";
-        public const string PLUGIN_NAME = "com.mayhan_gm.mycopunk.moreandquickloadouts";
-        public const string PLUGIN_VERSION = "1.0.4";
-    }
-}
